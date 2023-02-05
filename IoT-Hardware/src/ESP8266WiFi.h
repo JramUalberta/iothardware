2401426ef028bf5915876a52f9a0e47685d55fc5
Philip Zhu <philipzhu776@gmail.com>
2023-02-04 17:53:38 -0700

    Removed sensitive data

--- IoT-Hardware/src/ESP8266WiFi.h
+++
@@ -1,3 +0,0 @@
-// Insert your network credentials
-#define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
-#define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
--- IoT-Hardware/src/Firebase_ESP_Client.h
+++
@@ -1,6 +0,0 @@
-
-// Insert Firebase project API Key
-#define API_KEY "REPLACE_WITH_YOUR_FIREBASE_PROJECT_API_KEY"
-
-// Insert RTDB URLefine the RTDB URL */
-#define DATABASE_URL "REPLACE_WITH_YOUR_FIREBASE_DATABASE_URL" 
